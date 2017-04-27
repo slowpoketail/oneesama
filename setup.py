894652c10e7265c80ab335cff9bc1a4b7555b43b
slowpoke <mail+git@slowpoke.io>
2017-04-27 18:30:16 +0000

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='oneesama',
+    packages=['oneesama'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'oneesama = oneesama:main',
+        ],
+    },
+    install_requires=[
+        'flask',
+        'flask_peewee',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_requires=[
+        'pytest',
+    ],
+)
