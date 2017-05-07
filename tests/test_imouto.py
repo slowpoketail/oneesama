4332a89e1a10e8222eeb1b58e309d98eff7bf590
slowpoke <mail+git@slowpoke.io>
2017-05-07 17:45:03 +0000

    Add even more rudimentary tests for imouto.

--- tests/test_imouto.py
+++ tests/test_imouto.py
@@ -12,34 +12,40 @@ from flask_testing import LiveServerTestCase
 from flask import Flask
 from oneesama import config
 from oneesama.utils import urljoin
-import os.path
 
-from tempfile import mkdtemp
+import pytest
+import requests
+
 
 def create_app(self):
     app = Flask(__name__)
     app.config.from_object(config)
     app.config["TESTING"] = True
-    app.config["TEST_DIR"] = mkdtemp(prefix="oneesama-test:")
     app.config["DATABASE"] = {
-        "name": os.path.join(app.config["TEST_DIR"], "oneesama.db"),
+        "name": "/tmp/oneesama.db",
         "engine": "peewee.SqliteDatabase",
     }
     #app.config["LIVESERVER_PORT"] = 0
     return app
 
-class TestAPI(LiveServerTestCase):
+
+class TestFile(LiveServerTestCase):
 
     create_app = create_app
 
-
     def test_create(self):
-        #File.base_url = urljoin(self.get_server_url(), "/api/")
         f = File.create()
         assert hasattr(f, "id")
         assert hasattr(f, "content_uri")
-        a = Anime.create(name="foo", file_id=f.id)
-        assert hasattr(a, "id")
-        assert hasattr(a, "name")
-        assert hasattr(a, "file")
+        assert requests.get(f.uri).status_code == 200
+        assert f.content_uri == "/file/{}/content".format(f.id)
 
+    def test_unique(self):
+        f1 = File.create()
+        f2 = File.create()
+        assert f1.id != f2.id
+
+    def test_delete(self):
+        f = File.create()
+        f.delete()
+        assert requests.get(f.uri).status_code == 404
